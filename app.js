f9977cfa727f3f30d9d870a1d0b7d10ca2d57c74
Omar Ramadan <ramomar1992@outlook.com>
2021-01-27 15:09:06 +0200

    added a new line of code on js file

--- app.js
+++ app.js
@@ -13,3 +13,4 @@ if(confirmation == true){
 } else {
     alert("Please relode the page then type your name again!");
 }
+document.write('<h2>Your name will be added : <em>'+ userName +'</em></h2>But unfotunately when you relode the page it will be deleted again hahahaha<h2>Have A good day</h2>');
